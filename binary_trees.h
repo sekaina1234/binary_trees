95a1cc142842ed40d2154ec22eb11bd196158af7
sekaina1234 <soukainasaoud54@gmail.com>
2023-08-30 17:03:13 -0700

    9. Height

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,6 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
+#include <stddef.h>
 typedef struct binary_tree_s binary_tree_t;
 struct binary_tree_s
 {
@@ -18,4 +19,5 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
 #endif /* BINARY_TREES_H */
